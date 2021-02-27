86b38da2afdca8fe99158da4e63546f39fecfb66
Makhabat <maksatbekova.m@gmail.com>
2021-02-27 03:52:30 -0800

    exercises 4

---
+++ exercise_4.py
@@ -0,0 +1,15 @@
+"""
+    4. Create a program that asks the user for a number and then prints out a list of all the divisors of that number. 
+    (If you don’t know what a divisor is, 
+    it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+"""
+
+user_number = int(input("Give me a number: "))
+divisors_list = []
+
+numbers_list = list(range(1, user_number+1))
+for item in numbers_list:
+    if user_number % item == 0:
+        divisors_list.append(item)
+print(divisors_list)
+
